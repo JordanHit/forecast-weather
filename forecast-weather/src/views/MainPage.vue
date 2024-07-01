2fd40e24ed1e11344775043fb9129d87ae1270b3
i <fdgh@fgh.yyy>
2024-07-01 16:50:54 +0300

    add Axios as package and query

--- forecast-weather/src/views/MainPage.vue
+++ forecast-weather/src/views/MainPage.vue
@@ -6,7 +6,22 @@ export default {
     name: 'MainPage',
     data(){
         return{
-
+            responseData: [],
+        }
+    },
+    mounted(){
+        this.getweatherNow()
+    },
+    methods:{
+        getweatherNow(){
+            this.$http.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=Горки&appid=3b5862a364fe4969ed8947f60c67cf3e`)
+                .then((res) => {
+                    this.responseData = res.data
+                    console.log(this.responseData)
+                })
+                .catch((error) => {
+                    console.error(error)
+                });
         }
     }
 }
