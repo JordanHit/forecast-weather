c4a329d507fe0ccc7447dac7ffb0781bc58e0816
Q <40528352+JordanHit@users.noreply.github.com>
2024-07-03 03:02:27 +0300

    Change color themes and fonts

--- forecast-weather/src/App.vue
+++ forecast-weather/src/App.vue
@@ -19,23 +19,32 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Lobster&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+@import 'assets/css/bundle.css';
 
 
 :root {
-  --blue: #033043;
-  --dark: #121E31;
-  --skobeloff: #0A7273;
-  --white: #E9E3D5;
-  --yellow: #FDA521;
-  --dark-light: #22284F
-}
+  //--blue: #033043;
+  //--dark: #121E31;
+  //--skobeloff: #0A7273;
+  //--white-coffie: #E9E3D5;
+  //--yellow: #FDA521;
+  //--dark-light: #22284F;
+  //--grey-dark: #343a40;
+  //--grey-middle: #495057;
 
+  --white: #ffffff;
+  --grey: #9E9E9E;
+  --black: #1f1f1f;
+  --black-light: #292929;
+  --purpule: #8259c7;
+  --grey-light: #CCCCCC;
+}
 * {
   top: 0;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Inter', sans-serif;
+  font-family: "Raleway", sans-serif;
 }
 </style>
--- forecast-weather/src/views/MainPage.vue
+++ forecast-weather/src/views/MainPage.vue
@@ -1,12 +1,15 @@
 <template>
   <main>
-    <img src="//cdn.weatherapi.com/weather/64x64/day/113.png" alt="">
-    <h1>dsfd</h1>
+    <div class="">
+      <search-component :region="ipAddressData.region" :city="ipAddressData.city" :country="ipAddressData.country"></search-component>
+    </div>
   </main>
 </template>
 <script>
+import SearchComponent from "@/components/SearchComponent.vue";
 export default {
   name: 'MainPage',
+  components:{SearchComponent},
   data() {
     return {
       ipAddressData:[],
@@ -18,18 +21,20 @@ export default {
   },
   methods: {
     getIpAddress(){
-      this.$http.get(`https://api.geoapify.com/v1/ipinfo?apiKey=d763ad9cb40348d186e5fc0447b3392f`)
+      this.$http.get(`http://ipwho.is/?lang=ru`)
           .then((res) => {
             this.ipAddressData = res.data
             console.log(this.ipAddressData)
-            this.getWeatherNow(this.ipAddressData.location.latitude,this.ipAddressData.location.longitude)
+            this.getWeatherNow(this.ipAddressData.latitude,this.ipAddressData.longitude)
           })
           .catch((error) => {
             console.error(error)
           });
     },
     getWeatherNow(lat,lon) {
-      this.$http.get(`api/current.json?key=0e2bd886eb124156bcc192102240107&q=${lat},${lon}&lang=ru`)
+      this.$http.get(`/api/forecast.json?key=0e2bd886eb124156bcc192102240107&days=10&q=${lat},${lon}&lang=ru`)
+      // this.$http.get(`/api/weather?lat=${lat}&lon=${lon}`)
+      // this.$http.get(`/api/current.json?key=0e2bd886eb124156bcc192102240107&q=${lat},${lon}`)
           .then((res) => {
             this.responseData = res.data
             console.log(this.responseData)
