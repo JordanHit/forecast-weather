50b146b17ec58743989446c3e21bb90a77dd4bcb
Q <40528352+JordanHit@users.noreply.github.com>
2024-07-03 03:02:41 +0300

    Change proxy

--- forecast-weather/vite.config.js
+++ forecast-weather/vite.config.js
@@ -12,5 +12,22 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/api/weather': {
+        target: 'https://api.weather.yandex.ru/v2/forecast',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/weather/, ''),
+        headers: {
+          'X-Yandex-API-Key': 'db55f3b5-c953-47d6-b50c-7a1056bf2712', // Замените на ваш реальный API ключ
+        },
+      },
+      '/api': {
+        target: 'http://api.weatherapi.com/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
